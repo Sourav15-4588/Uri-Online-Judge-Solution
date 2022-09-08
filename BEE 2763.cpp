af5f759f9261d7ad48ec85997fda56293312ae06
Sourav <111035583+Sourav15-4588@users.noreply.github.com>
2022-09-08 14:03:58 +0600

    Add files via upload

---
+++ BEE 2763.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z,d;
+    char dot,desh;
+    scanf("%d %c %d %c %d %c %d" ,&x,&dot,&y,&dot,&z,&desh,&d);
+    printf("%03d\n%03d\n%03d\n%02d\n",x,y,z,d);
+    return 0;
+}
